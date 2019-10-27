76ab194f5d6cc0674d4bec572858d1579dc63a5a
shizhe <2032806791@qq.com>
2019-10-27 18:33:41 +0800

    init project

---
+++ src/main/java/cn/shizhe/community/controller/HelloController.java
@@ -0,0 +1,16 @@
+package cn.shizhe.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("hello")
+    public String hello() {
+        return  "hello";
+    }
+}
